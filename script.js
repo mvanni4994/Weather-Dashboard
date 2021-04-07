ad728afa33130c53591fbaa9475c037822dd0b71
Matthew Vanni <mvanni4994@gmail.com>
2021-04-07 04:09:24 -0400

    completed index

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
 
-    const APIKey = "a575879ffdb6f532351ea0bb4a765aa7"
-    let cities = []
-}
+    const APIKey = "a575879ffdb6f532351ea0bb4a765aa7";
+    let cities = [];
+});

a456f9da1e78b0a4fbe6940a53bcd83f17d92b00
Matthew Vanni <mvanni4994@gmail.com>
2021-04-08 03:38:57 -0400

    added AJAX shells

--- script.js
+++ script.js
@@ -1,5 +1,58 @@
+const APIKey = "a575879ffdb6f532351ea0bb4a765aa7";
+
 $(document).ready(function(){
 
-    const APIKey = "a575879ffdb6f532351ea0bb4a765aa7";
     let cities = [];
+    
+   
+// need queryUV, queryForecast, queryCurrent
+
+    function uvIndex(lat, lon){
+        var uvAPIroute= "https://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey
+    
+        // ajax call for uv
+        $.ajax({
+            url: uvAPIroute,
+            method: "GET"
+        }) .then(function(response){
+            console.log(response)
+        })
+    };
+
+    function forecast(userCity){
+        var forecast= "https://api.openweathermap.org/data/2.5/forecast?q=" + userCity + "&appid=" + APIKey
+        // ajax call for forecast
+
+        $.ajax({
+            url:forecast,
+            method: "GET"
+        }) .then(function(response){
+            console.log(response)
+        })
+    };
+
+    function current(cityName){
+        var city= "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey
+        //  ajax call for current weather
+        $.ajax({
+            url:city,
+            method: "GET"
+        }) .then(function(response){
+            console.log(response)
+        })
+    };
+
+    $("#search").on("click", function(event) {
+        event.preventDefault();
+        var cityInput= $("#city-input").val().trim();
+        console.log(cityInput);
+        if (cityInput){
+            cities.push(cityInput.charAt(0).toUpperCase() + city.slice(1));
+        } 
+        $("#city-input").val("");
+        // ( "i've been clicked." );
+      });
 });
+
+
+
